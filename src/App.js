7d09cf8b6e660836cccb28643cc03a7dcb126994
jhalyn45 <e.renney80@gmail.com>
2025-06-22 18:51:47 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold">BlockGrindz Trivia Game</h1>
-      <p>Insert TriviaEngine and Leaderboard components here.</p>
+      <p>Which rapper is known as the "King of the South"?</p>
+      <ul>
+        <li>T.I.</li>
+        <li>Ludacris</li>
+        <li>Rick Ross</li>
+        <li>Gucci Mane</li>
+      </ul>
     </div>
   );
 }
